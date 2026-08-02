288e6d0881fdf45ea4405c7ee68e6996df7ce516
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-08-02 20:53:01 +0300

    feat: create nullable compatible insert statements

--- backend/db/query.sql
+++ backend/db/query.sql
@@ -1,4 +1,6 @@
---- SELECT ---
+------------------------
+-------- SELECT --------
+------------------------
 
 -- name: GetAllGenres :many
 SELECT *
@@ -18,7 +20,7 @@ SELECT *
 FROM expectations
 WHERE displayed_by_default = true;
 
--- name: GetAllDefaulImpressions :many
+-- name: GetAllDefaultImpressions :many
 SELECT *
 FROM impressions
 WHERE displayed_by_default = true;
@@ -32,13 +34,13 @@ WHERE displayed_by_default = true;
 SELECT *
 FROM strengths
 WHERE name =
-$1;
+      $1;
 
 -- name: GetSpecificAnnoyances :one
 SELECT *
 FROM annoyances
 WHERE name =
-$1;
+      $1;
 
 -- name: GetAllDefaultAnnoyances :many
 SELECT *
@@ -50,45 +52,51 @@ SELECT *
 FROM setting
 WHERE displayed_by_default = true;
 
---- EXECUTE ---
--- name: CreateAuthor :exec
--- INSERT INTO authors (name, bio)
--- VALUES (sqlode.arg(author_name), sqlode.narg(bio));
+-- name: GetUserAccountIdByEmail :one
+SELECT id
+FROM user_account
+WHERE email = $1;
 
+-----------------------
+------- EXECUTE -------
+-----------------------
 
 -- name: AddTvShow :exec
-INSERT INTO tv_show (
-    title, year, title_in_latin, setting, format_id,
-    post_insight_id,
-    extended_opinion,
-    moral_understanding,
-    finished,
-    expectations_id,
-    genres_id,
-    mood_id,
-    strengths_id,
-    annoyances_id,
-    impressions_id,
-    visual_rate,
-    dialogues_rate,
-    characters_rate,
-    story_rate, atmosphere_rate, moral_depth_rate, originality_rate
-)
-VALUES (
-    sqlode.arg(title), sqlode.arg(year), sqlode.arg(title_in_latin),
-    sqlode.arg(setting), sqlode.arg(format_id), sqlode.arg(post_insight_id),
-    sqlode.arg(extended_opinion), sqlode.arg(moral_understanding),
-    sqlode.arg(finished), sqlode.arg(expectations_id), sqlode.arg(genres_id),
-    sqlode.arg(mood_id), sqlode.arg(strengths_id), sqlode.arg(annoyances_id),
-    sqlode.arg(impressions_id),
-    sqlode.arg(visual_rate),
-    sqlode.arg(dialogues_rate),
-    sqlode.arg(characters_rate),
-    sqlode.arg(story_rate),
-    sqlode.arg(atmosphere_rate),
-    sqlode.arg(moral_depth_rate),
-    sqlode.arg(originality_rate)
-);
+INSERT INTO tv_show (title, year, title_in_latin, setting, format_id,
+                     post_insight_id,
+                     extended_opinion,
+                     moral_understanding,
+                     finished,
+                     expectations_id,
+                     genres_id,
+                     mood_id,
+                     user_account_id,
+                     strengths_id,
+                     annoyances_id,
+                     impressions_id,
+                     visual_rate,
+                     dialogues_rate,
+                     characters_rate,
+                     story_rate, atmosphere_rate, moral_depth_rate, originality_rate)
+VALUES (sqlode.arg(title), sqlode.arg(year), sqlode.narg(title_in_latin),
+        sqlode.narg(setting), sqlode.narg(format_id), sqlode.narg(post_insight_id),
+        sqlode.narg(extended_opinion), sqlode.narg(moral_understanding),
+        sqlode.narg(finished), sqlode.narg(expectations_id), sqlode.narg(genres_id),
+        sqlode.narg(mood_id), sqlode.arg(user_account_id), sqlode.narg(strengths_id), sqlode.narg(annoyances_id),
+        sqlode.narg(impressions_id),
+        sqlode.narg(visual_rate),
+        sqlode.narg(dialogues_rate),
+        sqlode.narg(characters_rate),
+        sqlode.narg(story_rate),
+        sqlode.narg(atmosphere_rate),
+        sqlode.narg(moral_depth_rate),
+        sqlode.narg(originality_rate));
+
+
+--name: addUserCustomShowStrength :exec
+INSERT INTO annoyances(language_code, name)
+VALUES (sqlode.arg(language_code), sqlode.arg(name));
 
 --name: addUserCustomShowAnnoyance :exec
 INSERT INTO annoyances(language_code, name)
+VALUES (sqlode.arg(language_code), sqlode.arg(name));
