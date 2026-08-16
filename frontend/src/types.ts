79be84f9f73c457e9d0df3b1d804826166b46176
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-08-16 17:45:31 +0300

    fix: security issue with types. implicit style

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,59 +1,10 @@
-
-export interface Genre {
-  id: number,
-  name: string,
-  language_code: string,
-}
-
-
-export interface Mood {
-  id: number,
-  name: string,
-  language_code: string,
-}
-
-
-export interface Expectations {
-  id: number,
-  state: string,
-  language_code: string,
-}
-
-
 export interface Impressions {
-  id: number,
   state: string,
   order_number: number,
-  language_code: string,
-}
-
-export interface Format {
-  id: number,
-  name: string,
-  language_code: string,
-}
-
-export interface Strengths {
-  id: number,
-  name: string,
-  language_code: string,
-}
-
-
-export interface Annoyances {
-  id: number,
-  name: string,
-  language_code: string,
-}
-
-export interface Setting {
-  id: number,
-  name: string,
-  language_code: string,
 }
 
 export interface Pacing {
-  id: number,
+  id: number | null,
   beginning: number,
   ending: number,
   middle: number,
@@ -70,8 +21,8 @@ export interface Pacing {
 
 
 export interface TvShow {
-  id: number
-  title: string
+  id: number | null
+  title: string | null
   year: number | null
   title_in_latin: string | null
   country: string | null
@@ -85,43 +36,20 @@ export interface TvShow {
   atmosphere_rate: number | null
   moral_depth_rate: number | null
   originality_rate: number | null
-  format: Format,
-  pacing: Pacing,
-  genre: Genre[],
-  expectations: Expectations[],
-  mood: Mood[],
-  impressions: Impressions[],
-  annoyances: Annoyances[],
-  strengths: Strengths[],
-  setting: Setting[],
+  format: string | null
+  pacing: Pacing | null
+  genre: string[]
+  expectations: string[]
+  mood: string[]
+  impressions: Impressions[]
+  annoyances: string[]
+  strengths: string[]
+  setting: string[]
 }
 
-export interface PostInsight {
+export interface PostInsight extends TvShow {
   id: number
   rewatched: boolean
   forgettability: number
-  title: string
-  year: number | null
-  title_in_latin: string | null
-  country: string | null
-  extended_opinion: string | null
-  moral_understanding: string | null
-  finished: boolean
-  visual_rate: number | null
-  dialogues_rate: number | null
-  characters_rate: number | null
-  story_rate: number | null
-  atmosphere_rate: number | null
-  moral_depth_rate: number | null
-  originality_rate: number | null
-  format: Format,
-  pacing: Pacing,
-  genre: Genre[],
-  expectations: Expectations[],
-  mood: Mood[],
-  impressions: Impressions[],
-  annoyances: Annoyances[],
-  strengths: Strengths[],
-  setting: Setting[],
 }
 
